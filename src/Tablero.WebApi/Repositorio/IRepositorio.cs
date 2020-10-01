d9836b3110242fa7ecdd9283a2c17bdfdc2f5a53
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2020-09-30 22:20:30 -0500

    creacion del repositorio

---
+++ src/Tablero.WebApi/Repositorio/IRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tablero.WebApi.Models;
+
+namespace Tablero.WebApi.Repositorio
+{
+    public interface IRepositorio
+    {
+        public Task<T> GetObjet<T>(Guid id) where T : class;
+
+        public Task<IEnumerable<T>> GetAllObjet<T>() where T : class;
+    }
+}
---
+++ src/Tablero.WebApi/Repositorio/Repositorio.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tablero.WebApi.Context;
+
+namespace Tablero.WebApi.Repositorio
+{
+    public class Repositorio : IRepositorio
+    {
+        private readonly TableroContext tableroContext;
+
+        public Repositorio(TableroContext tableroContext)
+        {
+            this.tableroContext = tableroContext;
+        }
+
+        public async Task<IEnumerable<T>> GetAllObjet<T>() where T : class
+        {
+            try
+            {
+                return await this.tableroContext.Set<T>().ToListAsync();
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception("no se pudo traer las lista de entidades", e);
+            }
+        }
+
+        public async Task<T> GetObjet<T>(Guid id) where T : class
+        {
+            try
+            {
+                return await this.tableroContext.Set<T>().FindAsync(id);
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception("no se pudo traer la entidad", e);
+            }
+        }
+    }
+}
