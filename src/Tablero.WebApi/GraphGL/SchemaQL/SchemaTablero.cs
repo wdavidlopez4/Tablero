05e5d709f5cbb8971b45e387dc0ea8b54ade7885
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2020-10-06 17:21:23 -0500

    configuracion del esquema de QL

--- src/Tablero.WebApi/GraphGL/SchemaQL/SchemaTablero.cs
+++ src/Tablero.WebApi/GraphGL/SchemaQL/SchemaTablero.cs
@@ -1,16 +1,18 @@
-﻿using GraphQL.Types;
+﻿using GraphQL;
+using GraphQL.Types;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tablero.WebApi.GraphGL.Queries;
 
 namespace Tablero.WebApi.GraphGL.SchemaQL
 {
     public class SchemaTablero : Schema
     {
-        public SchemaTablero()
+        public SchemaTablero(IDependencyResolver resolver): base(resolver)
         {
-
+            this.Query = resolver.Resolve<Query>();
         }
     }
 }
